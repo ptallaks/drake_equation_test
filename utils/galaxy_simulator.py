a29b957f29ff4b97ba8e94fa1033440b51e9357b
Peter Tallaksen <93915254+ptallaks@users.noreply.github.com>
2023-03-16 10:19:17 +0900

    first part completed

---
+++ utils/galaxy_simulator.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from random import randint, uniform, random
+import math
+#=============================================================================
+# MAIN INPUT   
+
+# scale (radio bubble diameter) in light-years:
+SCALE = 225 # enter 225 to see Earth's radio bubble   
+
+# number of advanced civilizations from the Drake equation:
+NUM_CIVS = 15600000
+
+#=============================================================================”
